65503ba1689ab476ae6fb42452e19478819f9e3d
enigma01Z313 <f.ahmadyf94@gmail.com>
2022-07-25 18:58:08 +0430

    v 1.0.0

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+//setup public diractory
+const publicDirectoryPath = path.join(__dirname, "./public");
+app.use(express.static(publicDirectoryPath));
+
+//open this on browser
+//http://localhost:3000/filemanager/image.JPG
+
+
+//method 2
+//import dummy db
+const images = require("./db");
+app.get("/files/:id", (req, res) => {
+  const {path: imagePath} = images.find(item => item.id)
+  const imageAbsolutePath = path.resolve('./', './files', imagePath)
+  return res.sendFile(imageAbsolutePath)
+});
+
+
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
---
+++ db.js
@@ -0,0 +1,6 @@
+module.exports = [
+  {id: 1, path: 'image.JPG'},
+  {id: 10, path: 'image10.JPG'},
+  {id: 20, path: 'image20.JPG'},
+  {id: 5, path: 'image5.JPG'},
+]
