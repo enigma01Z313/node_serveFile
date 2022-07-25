58f0733fefda036be429b3fc21d4a4d2f8218ac0
enigma01Z313 <f.ahmadyf94@gmail.com>
2022-07-25 19:20:47 +0430

    v 1.0.1

--- app.js
+++ app.js
@@ -10,17 +10,14 @@ app.use(express.static(publicDirectoryPath));
 //open this on browser
 //http://localhost:3000/filemanager/image.JPG
 
-
 //method 2
 //import dummy db
 const images = require("./db");
 app.get("/files/:id", (req, res) => {
-  const {path: imagePath} = images.find(item => item.id)
+  const { path: imagePath } = images.find((item) => item.id === +req.params.id);
   const imageAbsolutePath = path.resolve('./', './files', imagePath)
   return res.sendFile(imageAbsolutePath)
 });
-
-
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
